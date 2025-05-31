fd979bf0fe18b1031cc9f5c43de13ff330fb4836
Shiv <khannashiv94@gmail.com>
2025-05-31 18:39:59 +0530

    test

---
+++ Project-1/file-operations.py
@@ -0,0 +1,14 @@
+def update_server_conf(file_path, key, value):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(file_path, 'w') as file:
+        for line in lines:
+            if key in line:
+                # Update the line with the new value
+                file.write(f"{key} = {value} \n")
+            else:
+                # Write the line as it is
+                file.write(line)
+
+update_server_conf('server.conf', 'MAX_CONNECTIONS', '500')
