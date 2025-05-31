2ea50f163b042f7cba08f96aa50434ea1497afce
Shiv <khannashiv94@gmail.com>
2025-05-31 11:45:47 +0530

    test

--- list_folders_files.py
+++ list_folders_files.py
@@ -1,20 +1,22 @@
 import os
 
-folders = input("Please provide list of folders with spaces - ").split()
- 
 # To get list of folders from user input
 # Example: folders = ['folder1', 'folder2', 'folder3']
 # print(folders)
 
+folders = input("Please provide list of folders with spaces - ").split()
+ 
 for folder in folders:
-    # print(folder)
+    print(f"Files present under {folder} directory.")
     try:
         files=os.listdir(folder)
     except FileNotFoundError:
         print(f"Folder '{folder}' does not exist.")
         continue
+    except PermissionError:
+        print(f"Permission denied for folder '{folder}'.")
+        continue
     for file in files:
-        print("Printing files in folder:", folder)
         print(file)
 
 
