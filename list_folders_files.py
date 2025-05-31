502e241d8e0cde009e91d990186c9965f8f48cbd
Shiv <khannashiv94@gmail.com>
2025-05-31 11:51:37 +0530

    test

--- list_folders_files.py
+++ list_folders_files.py
@@ -1,13 +1,9 @@
 import os
-
-# To get list of folders from user input
-# Example: folders = ['folder1', 'folder2', 'folder3']
-# print(folders)
-
+# This script lists files in multiple directories specified by the user.
 folders = input("Please provide list of folders with spaces - ").split()
  
 for folder in folders:
-    print(f"Files present under {folder} directory.")
+    print(f"*** Files present under {folder} directory ***")
     try:
         files=os.listdir(folder)
     except FileNotFoundError:
@@ -20,3 +16,13 @@ for folder in folders:
         print(file)
 
 
+# This code snippet prompts the user to enter a list of folder names, separated by spaces. 
+# The input is read as a single string and then split into a list of folder names using the split() method, which by default splits on whitespace. 
+# This allows the user to specify multiple folders in one line.
+# The code then iterates over each folder name in the list. For each folder, it prints a message indicating that it will list the files under that directory. 
+# It attempts to retrieve the list of files in the folder using os.listdir(folder). If the folder does not exist, a FileNotFoundError is caught, and an error message is printed. 
+# If the program does not have permission to access the folder, a PermissionError is caught, and a different error message is shown.
+#  In both error cases, the loop continues to the next folder without crashing.
+# If the folder is accessible and exists, the code iterates through the list of files returned by os.listdir() and prints each file name.
+# This provides a simple way for users to see the contents of multiple directories in one run. One thing to note is that the code assumes the os module has already been imported; otherwise, it will raise a NameError. 
+# Also, folder names containing spaces will not be handled correctly, as the split will break them into separate entries.
