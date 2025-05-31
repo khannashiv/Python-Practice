39294dde54c8a12e3d9be9e20e0131a6d61d023f
Shiv <khannashiv94@gmail.com>
2025-05-31 12:32:00 +0530

    test

--- sample_dictionary.py
+++ sample_dictionary.py
@@ -1,14 +1,20 @@
+# This code defines a list called ec2_info, where each element is a dictionary representing information about an EC2 instance.
+
 ec2_info = [
     {
-        id="1",
-        type="t2.micro"
+        "id": "1",
+        "type": "t2.micro"
     },
     {
-        id="2",
-        type="t2.small"
+        "id": "2",
+        "type": "t2.small"
     },
     {
-        id="3",
-        type="t2.medium"
+        "id": "3",
+        "type": "t2.medium"
     }
 ]
+
+print(ec2_info[1])
+print(ec2_info[2]["type"])
+print(ec2_info[0]["id"])
