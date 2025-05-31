e2de95db114379d290329930a35d4d57a5039774
Shiv <khannashiv94@gmail.com>
2025-05-31 16:19:21 +0530

    test

---
+++ get_pull_request_2.py
@@ -0,0 +1,30 @@
+import requests
+url="https://api.github.com/repos/kubernetes/kubernetes/pulls"
+response=requests.get(url)
+
+print(response)
+print(type(response))
+
+output = response.json()
+print(type(output))
+
+# Initialize a dictionary to track pull request counts for each user
+user_count = {}
+
+for pr in output:
+    #print(f"User who made pull request: {pr['user']['login']}")
+    user_login = pr['user']['login']
+
+    # If user is already in dictionary, increment their count
+    if user_login in user_count:
+        user_count[user_login] += 1
+    else:
+        # Otherwise, add the user to the dictionary with an initial count of 1
+        user_count[user_login] = 1
+
+# Print the count of pull requests made by each user
+print("Pull request counts for all users:")
+for user, count in user_count.items():
+    print(f"{user}: {count}")
+
+    
