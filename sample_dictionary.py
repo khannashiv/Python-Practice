9f7610a9c448935a27fb4d6bf44ec6fe34eb74bc
Shiv <khannashiv94@gmail.com>
2025-05-31 12:20:28 +0530

    test

---
+++ sample_dictionary.py
@@ -0,0 +1,14 @@
+ec2_info = [
+    {
+        id="1",
+        type="t2.micro"
+    },
+    {
+        id="2",
+        type="t2.small"
+    },
+    {
+        id="3",
+        type="t2.medium"
+    }
+]
