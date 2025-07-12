334ede05e400c44208a32aa8d2bb0a4e51c2f955
Shiv <khannashiv94@gmail.com>
2025-07-12 13:40:58 +0530

    test

--- Practice-IQ/demo.py
+++ Practice-IQ/demo.py
@@ -21,6 +21,7 @@
 # Sol 2.
 
 # M0 || Sorting in Ascending order.
+
 # my_list=[11, 12, 1, 7, 10]
 
 # for i in range(len(my_list)):
@@ -31,12 +32,14 @@
 # print(my_list)
 
 # M1 || Using inbuilt function || Sorting in Ascending order.
+
 # my_list=[11, 12, 1, 7, 10]
 
 # sorted_list=sorted(my_list)
 # print(sorted_list)
 
-# # M2 || By initializing empty array || Sorting in Ascending order.
+# M2 || By initializing empty array || Sorting in Ascending order.
+
 # my_list=[11, 12, 1, 7, 10]
 
 # emp=[]
@@ -189,18 +192,133 @@
 
 # Method 3 : Using recursion.
 
-def fib(n):
-    if n <=0:
-        print("Please enter positive natural number. ")
-    elif n==1:
-        return 0
-    elif n==2:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+# def fib(n):
+#     if n <=0:
+#         print("Please enter positive natural number. ")
+#     elif n==1:
+#         return 0
+#     elif n==2:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
 
-n=int(input("Please enter your input. "))
-print(fib(n)) # To print final element of fibonacci series.
+# n=int(input("Please enter your input. "))
+# print(fib(n)) # To print final element of fibonacci series.
 
-# for i in range(n): # To print entire series till the value of n.
-#     print(fib(i))
+# # for i in range(n): # To print entire series till the value of n.
+# #     print(fib(i))
+
+# Ques 7. How to reverse a list, string, number using in built function & without inbuilt function & also check 
+# palindrome case whereever possible ?
+
+# Sol 7:
+
+############# For List / Array in python ######################################################
+
+# Method 1 & Condition 1.... Using inbuilt function.
+
+# my_list=[1, 2, 3, 2, 1]
+
+# new_list=my_list[::-1]
+# print(new_list)
+
+# Method 2 & Condition 2.... Without using inbuilt function.
+
+# start = 0                   # Initialize start with index 0
+# end = len(my_list) - 1      # Initialize end with the index of the last element
+
+# while start < end:
+#     my_list[start], my_list[end] = my_list[end], my_list[start]  # Swap the elements at the start and end.
+
+#     # Move the pointers towards the center
+#     start += 1
+#     end -= 1
+
+# print(my_list)
+
+# Method 3 & Condition 3.....  Check if given list is pallindrome or not
+
+# start = 0
+# end = len(my_list)-1
+# while start < end:
+#     if my_list[start] != my_list[end]:
+#         print("The given list is not Pallindrome. ")
+#         break
+#     start += 1
+#     end -= 1
+# else:
+#     print("Given list is Pallindrome. ")
+
+############# For String in python ####################################################################
+
+# Method 1 & Condition 1.... Using inbuilt function. [ Reversing the string .]
+
+# my_str = "malayalam"  # Tesed with abcd, madam etc.
+# new_str = ""
+
+# new_str = my_str[::-1]
+# print(new_str)
+
+# Method 2 & Condition 2.... Without using inbuilt function.
+
+# for i in my_str:
+#     #print(i)
+#     new_str = i + new_str
+# print(new_str)
+
+# Method 3 & Condition 3.....  Check if given string is pallindrome or not
+
+# if new_str == my_str:
+#     print("The original string is pallindrome. ")
+# else:
+#     print("The original string is not pallindrome. ")
+
+
+############# For Number / Integer in python ################################################# 
+
+# Method 1 & Condition 1.... Using inbuilt function. [ Reversing the Number.]
+
+# my_num = str(123)
+# rev_num=int(my_num[::-1])
+# print(rev_num)
+
+# Method 2 & Condition 2.... Without using inbuilt function.
+
+# my_num = 343
+# temp = my_num
+# rev = 0
+
+# while my_num > 0:
+#     rem = my_num%10
+#     rev = rev*10 + rem
+#     my_num = my_num//10
+
+# print(rev)
+
+# # Method 3 & Condition 3.....  Check if given number is pallindrome or not.
+
+# if ( rev == temp ):
+#     print("Given number is palindrome. ")
+# else:
+#     print("Given number is not pallindrome. ")
+
+# Ques 8 : Check if the given number is Armstrong or not ?
+
+# Sol 8 :
+
+test_num=153
+temp=test_num
+rev=0
+order= len(str(test_num))
+
+while test_num > 0:
+    rem = test_num%10
+    rev = rev + rem**order
+    test_num = test_num//10
+
+print(rev)
+
+if ( rev == temp ):
+    print("The given number is armstrong number. ")
+else:
+    print("The given number is not armstrong number. ")
