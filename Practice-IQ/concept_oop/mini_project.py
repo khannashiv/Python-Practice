39820108dd133dea158ab48c7f740c034eb5f51b
Shiv <khannashiv94@gmail.com>
2025-07-19 19:54:27 +0530

    test

--- Practice-IQ/concept_oop/mini_project.py
+++ Practice-IQ/concept_oop/mini_project.py
@@ -108,14 +108,3 @@ if __name__ == "__main__":
 
     # List available books again after issuing/returning
     my_lib.list_books()
-
-    # Points to note.
-
-        # Passing an instance: Here in this code, we're passing instances of the book class (like book_1) to add_book, 
-        # which the library can then store.
-
-        # You do not need to change the parameter name (book) in the add_book method if you change the class 
-        # name from book to Book.
-
-        # The parameter book in add_book will still work as it is, 
-        # because it’s just a name referring to the instance of the Book class passed to the method.
